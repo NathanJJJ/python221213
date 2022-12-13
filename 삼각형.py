f083e8f4ce6d27c4ce31d1934deb535aa84d3456
NathanJJJ <keprin@proton.me>
2022-12-13 12:01:43 +0900

    first

---
+++ 삼각형.py
@@ -0,0 +1,12 @@
+# 삼각형
+
+for i in [1,2,3,4,5,6,7,8,9,10]:
+    print("*" * i)
+
+lst = [1,2,3,4,5,6,7,8,9,10]
+for item in lst:
+    if item > 5:
+        break
+    print("item:{0}".format(item))
+
+
